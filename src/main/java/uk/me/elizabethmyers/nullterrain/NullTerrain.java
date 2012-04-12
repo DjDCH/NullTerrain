4df6e7a9e6c046fadea1211c1e27a8491345366e
DjDCH <bukkit.coding@djdch.com>
2012-04-12 00:53:27 -0400

    Removed unused imports.

--- src/main/java/uk/me/elizabethmyers/nullterrain/NullTerrain.java
+++ src/main/java/uk/me/elizabethmyers/nullterrain/NullTerrain.java
@@ -1,16 +1,6 @@
 package uk.me.elizabethmyers.nullterrain;
 
-import java.util.List;
-import java.util.logging.Logger;
-import java.util.Collections;
-
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.Server;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NullTerrain extends JavaPlugin
